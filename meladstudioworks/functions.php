c5d523e680644db552f35c54c4f67c01a03a2f68
Paul Kehrer <pkehrer@cimpress.com>
2016-01-31 13:00:13 -0500

    Adding plugin validator to functions.php. Removing extraneous files.

--- meladstudioworks/functions.php
+++ meladstudioworks/functions.php
@@ -1,14 +1,14 @@
 <?php
-
-
-
-
+// Load TGMP plugin recommender library
+require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
 
 if (function_exists('register_nav_menus')) {
 	register_nav_menu( 'secondary', __( 'Secondary Menu', 'meladstudioworks' ) );
 }
 
 
+add_action('tgmpa_register', 'required_plugins');
+add_action("customize_register", "modify_theme_options");
 
 class custom_nav_walker extends Walker_Nav_Menu
 {
@@ -55,6 +55,34 @@ function modify_theme_options($wp_customize)
 	$wp_customize->remove_section("background_image");
 }
 
-add_action("customize_register", "modify_theme_options");
-
+function required_plugins()
+{
+	$plugins = array(
+		array(
+			'name'      => 'Multiple content blocks',
+			'slug'      => 'multiple-content-blocks',
+			'required'  => true,
+		),
+		array(
+			'name'      => 'Official StatCounter Plugin',
+			'slug'      => 'official-statcounter-plugin-for-wordpress',
+			'required'  => false,
+		),
+	);
+	
+	$config = array(
+		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'parent_slug'  => 'themes.php',            // Parent menu slug.
+		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => ''
+	);
+	
+	tgmpa( $plugins, $config );
+}
 ?>
